3022ccc198764f5a6fe256271ac0e8c9d44a46e4
DonTheChimera <donaldtbradfield@gmail.com>
2019-02-05 16:24:17 -0600

    publish to gitpages

--- server/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    AUTH0_DOMAIN: 'dtbradfield.auth0.com',
-    AUTH0_API_AUDIENCE: 'http://localhost:8083/api/', 
-    MONGO_URI: 'mongodb://<dbuser>:<dbpassword>@ds119765.mlab.com:19765/dtb-professional-portfolio'
-};
